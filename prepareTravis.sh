9f8fe97140ef89a2c0fde1146872676de9ed41c5
xyezir <xyezir@gmail.com>
2016-08-31 19:00:21 +0800

    add commands & react tmpl

--- prepareTravis.sh
+++ prepareTravis.sh
@@ -4,18 +4,6 @@ gcc -v || ln -s $(which gcc-4.8) /usr/bin/gcc
 gcc -v || exit 1
 
 filename="mDNSResponder-561.1.1"
-#orientdb="orientdb-community-2.1.9"
-#wget "http://orientdb.com/download.php?email=unknown@unknown.com&file=${orientdb}.tar.gz&os=linux" -O ${orientdb}.tar.gz
-
-
-#tar xzf "${orientdb}.tar.gz"
-#cd ${orientdb}
-#sed -i"" -e 's/<users>/<users><user resources="*" password="happy" name="root"\/>/g' config/orientdb-server-config.xml
-#cat config/orientdb-server-config.xml
-#./bin/server.sh &
-#sleep 2
-#curl -X POST http://root:happy@localhost:2480/database/koala-puree-test-test/plocal
-#cd ..
 
 wget "http://ftp.gnu.org/gnu/bison/bison-2.7.tar.gz"
 tar xzf "bison-2.7.tar.gz"
